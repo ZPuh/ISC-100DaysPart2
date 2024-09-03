4ca3bc10e8903dcf1d5e6de57736e5b9437d2a23
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-09-03 20:29:08 +0800

    Syntax C++

---
+++ Days002.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	// Mencetak Hello World
+	cout << "Hello World \n";
+	cout << "Muhammad Zuhdi" << "\n";
+	cout << "Majene" << endl;
+	cout << "Teknik \t";
+	cout << "Informatika \n";
+	cout << "hidup\\mati \n";
+	cout << "Petik \"" << "\n";
+	/*
+	Belajar c++
+	*/
+	
+	return 0;
+}
