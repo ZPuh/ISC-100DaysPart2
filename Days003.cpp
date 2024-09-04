90fcec1fdc540f99d0898ab8664fd0b28ded437a
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-09-04 18:40:31 +0800

    Variabel

---
+++ Days003.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int myNum = 100;
+	cout << myNum << "\n";
+	
+	myNum = 1000;
+	cout << myNum << "\n";
+	
+	double myDouble = 1.23;
+	cout << myDouble << "\n";
+	
+	char myChar = 'Z';
+	cout << myChar << "\n";
+	
+	bool myBool = true;
+	cout << boolalpha << myBool << "\n";
+	
+	string myStr = "Zuhdi";
+	cout << myStr << "\n";
+	cout << "My name is " << myStr << "\n";
+	
+	int x = 10;
+	int y = 20;
+	int sum = x + y;
+	
+	cout << sum << "\n";
+	
+	int z = 30;
+	cout << x + y + z << "\n";
+	
+	// Constanta
+	const double PHI = 3.14;
+	cout << PHI << "\n";
+	
+	return 0;
+}
