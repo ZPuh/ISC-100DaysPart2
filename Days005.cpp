cc9ac8963f71c9ef8da81644210a77e65cca0755
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-09-06 17:47:22 +0800

    Data Types

---
+++ Days005.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	int intVar = 100;
+	float floatVar = 100.0;
+	double douVar = 100.0;
+	char chVar = '1';
+	bool blVar = false;
+	string strVar = "Zuhdi";
+	
+	cout << intVar << "\n";
+	cout << floatVar << "\n";
+	cout << douVar << "\n";
+	cout << chVar << "\n";
+	cout << blVar << "\n";
+	cout << strVar << "\n";
+	
+	return 0;
+}
