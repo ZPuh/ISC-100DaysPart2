d5a4aa14631c5cb9a9b66d0ae45c20ff08c23e14
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-09-09 20:08:42 +0800

    Math

---
+++ Days008.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm> 
+#include <cmath>
+using namespace std;
+
+int main(){
+	cout << max(5,10) << "\n";
+	cout << min(5,10) << "\n";
+	cout << sqrt(25) << "\n";
+	cout << round(2.5) << "\n";
+	cout << log(25) << "\n";
+	return 0;
+}
