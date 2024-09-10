b75a9666494910897e274defaa23deefe9d255d9
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-09-10 22:53:56 +0800

    Boolean

---
+++ Days009.java
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	bool isCodingFun = true;
+  	bool isFishTasty = false;
+  	cout << isCodingFun << "\n";
+  	cout << isFishTasty << "\n";
+  	
+  	int myAge = 25;
+ 	int votingAge = 18;
+
+	if (myAge >= votingAge) {
+    cout << "Old enough to vote!";
+  	} else {
+    cout << "Not old enough to vote.";
+ 	 }
+ 
+	return 0;
+}
